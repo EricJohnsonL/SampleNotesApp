27beffcbdc03748eb8002b200dc05fae8f5852d1
Eric Johnson <JohnsonRC8@gmail.com>
2018-04-02 02:37:49 +0700

    implement content provider

--- app/src/main/java/com/ericjohnson/mynotesapp/MainActivity.java
+++ app/src/main/java/com/ericjohnson/mynotesapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ericjohnson.mynotesapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,14 +12,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import com.ericjohnson.mynotesapp.Entity.Note;
 import com.ericjohnson.mynotesapp.adapter.NoteAdapter;
-import com.ericjohnson.mynotesapp.db.NoteHelper;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 import static com.ericjohnson.mynotesapp.FormAddUpdateActivity.REQUEST_UPDATE;
+import static com.ericjohnson.mynotesapp.db.DatabaseContract.CONTENT_URI;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,9 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ProgressBar progressBar;
     FloatingActionButton fabAdd;
 
-    private LinkedList<Note> list;
+    private Cursor list;
     private NoteAdapter adapter;
-    private NoteHelper noteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +40,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
         fabAdd = (FloatingActionButton) findViewById(R.id.fab_add);
         fabAdd.setOnClickListener(this);
-
-        noteHelper = new NoteHelper(this);
-        noteHelper.open();
-
-        list = new LinkedList<>();
 
         adapter = new NoteAdapter(this);
         adapter.setListNotes(list);
@@ -70,32 +61,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private class LoadNoteAsync extends AsyncTask<Void, Void, ArrayList<Note>> {
+    private class LoadNoteAsync extends AsyncTask<Void, Void, Cursor> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             progressBar.setVisibility(View.VISIBLE);
-
-            if (list.size() > 0) {
-                list.clear();
-            }
         }
 
         @Override
-        protected ArrayList<Note> doInBackground(Void... voids) {
-            return noteHelper.query();
+        protected Cursor doInBackground(Void... voids) {
+            return getContentResolver().query(CONTENT_URI,null,null,null,null);
         }
 
         @Override
-        protected void onPostExecute(ArrayList<Note> notes) {
+        protected void onPostExecute(Cursor notes) {
             super.onPostExecute(notes);
             progressBar.setVisibility(View.GONE);
 
-            list.addAll(notes);
+            list=notes;
             adapter.setListNotes(list);
             adapter.notifyDataSetChanged();
 
-            if (list.size() == 0) {
+            if (list.getCount() == 0) {
                 showSnackbarMessage("Tidak ada data saat ini");
             }
         }
@@ -108,20 +95,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (resultCode == FormAddUpdateActivity.RESULT_ADD) {
                 new LoadNoteAsync().execute();
                 showSnackbarMessage("Satu item berhasil ditambahkan");
-                rvNotes.getLayoutManager().smoothScrollToPosition(rvNotes, new RecyclerView.State(), 0);
             }
         } else if (requestCode == REQUEST_UPDATE) {
 
             if (resultCode == FormAddUpdateActivity.RESULT_UPDATE) {
                 new LoadNoteAsync().execute();
                 showSnackbarMessage("Satu item berhasil diubah");
-                int position = data.getIntExtra(FormAddUpdateActivity.EXTRA_POSITION, 0);
-                rvNotes.getLayoutManager().smoothScrollToPosition(rvNotes, new RecyclerView.State(), position);
             } else if (resultCode == FormAddUpdateActivity.RESULT_DELETE) {
-                int position = data.getIntExtra(FormAddUpdateActivity.EXTRA_POSITION, 0);
-                list.remove(position);
-                adapter.setListNotes(list);
-                adapter.notifyDataSetChanged();
+                new LoadNoteAsync().execute();
                 showSnackbarMessage("Satu item berhasil dihapus");
             }
         }
@@ -130,9 +111,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (noteHelper != null) {
-            noteHelper.close();
-        }
     }
 
     private void showSnackbarMessage(String message) {
